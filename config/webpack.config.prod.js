2dfc2e1f83630ddb6ae3143ce71a6d50f669c799
陈金辉 <chenjinhui@meizhitec.com>
2019-05-23 17:42:27 +0800

    service-worker只能放在根目录，且需要https支持

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -50,7 +50,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 
 // Note: defined here because it will be used more than once.
 const cssFilename = 'assets/css/[name].[contenthash:8].css'
-const swFilename = 'assets/js/service-worker.js'
+const swFilename = 'service-worker.js'
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
 // However, our output is structured with css, js and media folders.
